e992fc89b57714f98bac9fe2243684faf0ca9c14
Angel Rojas <angelsystem1387@gmail.com>
2015-02-01 19:24:51 -0500

    version api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
-Rails.application.routes.draw do
+require 'api_constraints'
+MarketPlaceApi::Application.routes.draw do
   namespace :api, defaults: { format: :json },
                               constraints: { subdomain: 'api' }, path: '/' do
 
+    scope module: :v1,
+              constraints: ApiConstraints.new(version: 1, defult: true) do
+
+    end
   end
 end
