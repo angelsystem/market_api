9eb42cdb6887deb115c27d4cd6c66e1702ba156b
Angel Rojas <angelsystem1387@gmail.com>
2015-02-01 22:49:25 -0500

    Updates application controller to prevent CSFR exception from being raised

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::UsersController do
+RSpec.describe Api::V1::UsersController, :type => :controller do
 
   before(:each) { request.headers['Accept'] = "application/vnd.marketplace.v1" }
 
