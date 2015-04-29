27fa2508ce39f7dc39cf5bad7849e0fb984571f0
angelsystem <angelsystem1387@gmail.com>
2015-04-28 21:38:44 -0500

    fix bug request and response

--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -1,22 +1,23 @@
 require 'rails_helper'
 
-class Authentication
+class Authentication < ActionController::Base
   include Authenticable
 end
 
-describe Authenticable do
+describe Authenticable, :type => :controller do
   let(:authentication) { Authentication.new }
   subject { authentication }
 
   describe "#current_user" do
-    # before do
-    #   @user = FactoryGirl.create :user
-    #   request.headers["Authorization"] = @user.auth_token
-    #   authentication.stub(:request).and_return(request)
-    # end
+    before do
+      @user = FactoryGirl.create :user
+      request.headers["Authorization"] = @user.auth_token
+      authentication.stub(:request).and_return(request)
+      # allow(authentication).to receive(:request).and_return(request)
+    end
 
     it "returns the user from the authorization header" do
-      # expect(authentication.current_user.auth_token).to eql @user.auth_token
+      expect(authentication.current_user.auth_token).to eql @user.auth_token
     end
   end
 
