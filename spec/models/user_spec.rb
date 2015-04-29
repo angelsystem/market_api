052c8a8da32ce381ee6b10c0b1f79ff79de2b0c8
angelsystem <angelsystem1387@gmail.com>
2015-04-28 23:39:14 -0500

    Finishes modeling the product model along with user associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,7 @@ RSpec.describe User, :type => :model do
   it { should validate_confirmation_of(:password) }
   it { should allow_value('example@domain.com').for(:email) }
 
+  it { should have_many(:products) }
 
   describe "#generate_authenticate_token!" do
     it "generates a unique token" do
@@ -29,11 +30,21 @@ RSpec.describe User, :type => :model do
       @user.generate_authentication_token!
       expect(@user.auth_token).not_to eql existing_user.auth_token
     end
-
-    # it "generates another token when one already has been taken" do
-    #   existing_user = FactoryGirl.create(:user, auth_token: "auniquetoken123")
-    #   @user.generate_authentication_token!
-    #   expect(@user.auth_token).not_to eql existing_user.auth_token
-    # end
   end
+
+  describe "#products association" do
+    before do
+      @user.save
+      3.times { FactoryGirl.create :product, user: @user }
+    end
+
+    it "destroys the associated products on self destruct" do
+      products = @user.products
+      @user.destroy
+      products.each do |product|
+        expect(Product.find(product)).to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+  end
+
 end
